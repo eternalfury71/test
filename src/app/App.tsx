930b6f6a164e4b06f780a2d896d3bbd1d1ff3dda
Adil Yerlan <adilyerlan71@gmail.com>
2024-08-06 12:57:15 +0500

    package remove

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,5 @@
-import { animated, useSpring } from "@react-spring/web";
 function App() {
-  const [springs, api] = useSpring(() => ({ from: { x: 0 } }));
-  const handleClick = () => {
-    api.start({
-      from: { x: 0 },
-      to: { x: 300 },
-    });
-  };
-  return (
-    <animated.div
-      onClick={handleClick}
-      style={{
-        width: 80,
-        height: 80,
-        background: "red",
-        borderRadius: 8,
-        ...springs,
-      }}
-    />
-  );
+  return <div>hello</div>;
 }
 
 export default App;
