228329841d22a2749d6d82e7e42769fec51d7606
Adil Yerlan <adilyerlan71@gmail.com>
2024-11-28 09:14:33 +0500

    test done

--- js/harvest.js
+++ js/harvest.js
@@ -1,107 +1,102 @@
 // LIST
 
 const data = [
-    {
-        link: '#chair.html',
-        title: 'Slim PRO',
-        desc: 'Cтул Slim PRO предназначено не только для работы за компьютером, но и для дополнения антуража помещения. Красиво выполненная конструкция не только изысканно смотрится.',
-        price: '83000',
-        img: 'images/stul_kresla/SlimPRO.png',
-        code: '6702',
-        parent: 'computer',
-        category: 'computer_chair',
-    },
-    {
-        link: '#chair.html',
-        title: 'Slim',
-        desc: 'Изящные, легкие, универсальные и эргономичные кресла Slim подойдут для кабинета руководителя. А различные модификации этой серии позволят оформить в едином стиле различные зоны офиса.',
-        price: '79000',
-        img: 'images/stul_kresla/slim.png',
-        code: '6101',
-        parent: 'computer',
-        category: 'computer_chair',
-    },
-    {
-        link: '#chair.html',
-        title: 'Slim DC',
-        desc: 'Кресло Slim DC - это офисное кресло для руководителя спинка и сидение которого выполнена из из мягкой сетки.',
-        price: '134100',
-        img: 'images/stul_kresla/GloryDC.png',
-        code: '6987',
-        parent: 'computer',
-        category: 'computer_chair',
-    },
-    {
-        link: '#chair.html',
-        title: 'Hi-tech',
-        desc: 'Модель Hi-tech изготовлена в модном дизайне, а значит, будет отлично смотреться в любом современном интерьере.',
-        price: '95500',
-        img: 'images/stul_kresla/Hi-tech.png',
-        code: '6203',
-        parent: 'computer',
-        category: 'computer_chair',
-    },
-    {
-        link: '#chair.html',
-        title: 'Hi-tech PRO',
-        desc: 'Профилированная спинка – спинка, имеет анатомически правильную форму, повторяющую естественный изгиб позвоночника.',
-        price: '125000',
-        img: 'images/stul_kresla/Hi-techPRO.png',
-        code: '6057',
-        parent: 'computer',
-        category: 'computer_chair',
-    },
-    {
-        link: '#chair.html',
-        title: 'Prestige DC',
-        desc: 'Утонченность и функциональность, высокое качество обивочных материалов и комплектующих – сочетание, достойное современного офисного кресла.',
-        price: '122000',
-        img: 'images/stul_kresla/PrestigeDC.png',
-        code: '6041',
-        parent: 'computer',
-        category: 'computer_chair',
-    },
-    {
-        link: '#chair.html',
-        title: 'Comfort DC',
-        desc: 'Многоцелевое кресло нового поколения, олицетворяет новые стандарты простоты, универсальной применимости, качества и комфорта.',
-        price: '97610',
-        img: 'images/stul_kresla/ComfortDC.png',
-        code: '6807',
-        parent: 'computer',
-        category: 'computer_chair',
-    },
+  {
+    link: "#chair.html",
+    title: "Slim PRO",
+    desc: "Cтул Slim PRO предназначено не только для работы за компьютером, но и для дополнения антуража помещения. Красиво выполненная конструкция не только изысканно смотрится.",
+    price: "83000",
+    img: "images/stul_kresla/SlimPRO.png",
+    code: "6702",
+    parent: "computer",
+    category: "computer_chair",
+  },
+  {
+    link: "#chair.html",
+    title: "Slim",
+    desc: "Изящные, легкие, универсальные и эргономичные кресла Slim подойдут для кабинета руководителя. А различные модификации этой серии позволят оформить в едином стиле различные зоны офиса.",
+    price: "79000",
+    img: "images/stul_kresla/slim.png",
+    code: "6101",
+    parent: "computer",
+    category: "computer_chair",
+  },
+  {
+    link: "#chair.html",
+    title: "Slim DC",
+    desc: "Кресло Slim DC - это офисное кресло для руководителя спинка и сидение которого выполнена из из мягкой сетки.",
+    price: "134100",
+    img: "images/stul_kresla/GloryDC.png",
+    code: "6987",
+    parent: "computer",
+    category: "computer_chair",
+  },
+  {
+    link: "#chair.html",
+    title: "Hi-tech",
+    desc: "Модель Hi-tech изготовлена в модном дизайне, а значит, будет отлично смотреться в любом современном интерьере.",
+    price: "95500",
+    img: "images/stul_kresla/Hi-tech.png",
+    code: "6203",
+    parent: "computer",
+    category: "computer_chair",
+  },
+  {
+    link: "#chair.html",
+    title: "Hi-tech PRO",
+    desc: "Профилированная спинка – спинка, имеет анатомически правильную форму, повторяющую естественный изгиб позвоночника.",
+    price: "125000",
+    img: "images/stul_kresla/Hi-techPRO.png",
+    code: "6057",
+    parent: "computer",
+    category: "computer_chair",
+  },
+  {
+    link: "#chair.html",
+    title: "Prestige DC",
+    desc: "Утонченность и функциональность, высокое качество обивочных материалов и комплектующих – сочетание, достойное современного офисного кресла.",
+    price: "122000",
+    img: "images/stul_kresla/PrestigeDC.png",
+    code: "6041",
+    parent: "computer",
+    category: "computer_chair",
+  },
+  {
+    link: "#chair.html",
+    title: "Comfort DC",
+    desc: "Многоцелевое кресло нового поколения, олицетворяет новые стандарты простоты, универсальной применимости, качества и комфорта.",
+    price: "97610",
+    img: "images/stul_kresla/ComfortDC.png",
+    code: "6807",
+    parent: "computer",
+    category: "computer_chair",
+  },
 ];
 
 // SHOW
 
-let computerChairList = document.getElementById('computerChairList_____SHOW');
-
-displayList(data, computerChairList);
+let computerChairList = document.getElementById("computerChairList_____SHOW");
 
 function displayList(array, uniqId) {
+  uniqId.innerHTML = "";
 
-    uniqId.innerHTML = "";
+  array.map((a) => {
+    let formatter = function (priceSum) {
+      let price = priceSum.toString();
+      let formattedPrice = "";
+      for (let i = 0; i < price.length; i++) {
+        if (i > 0 && i % 3 === 0) {
+          formattedPrice = " " + formattedPrice;
+        }
+        formattedPrice = price[price.length - 1 - i] + formattedPrice;
+      }
+      return formattedPrice;
+    };
 
-    array.map((a) => {
+    let productItem = document.createElement("div");
+    productItem.classList.add("product_item");
 
-        let formatter = function (priceSum) {
-            let price = priceSum.toString();
-            let formattedPrice = '';
-            for (let i = 0; i < price.length; i++) {
-                if (i > 0 && i % 3 === 0) {
-                    formattedPrice = ' ' + formattedPrice;
-                }
-                formattedPrice = price[price.length - 1 - i] + formattedPrice;
-            }
-            return formattedPrice;
-        };
-
-        let productItem = document.createElement('div');
-
-        productItem.classList.add("product_item");
-
-        productItem.innerHTML = `
+    productItem.innerHTML = `
         <a class="product_item_content" href="${a.link}">
             <img class="product_item_img" src="${a.img}" alt="Product">
             <div class="product_item_text">
@@ -112,13 +107,91 @@ function displayList(array, uniqId) {
         <div class="product_item_price">
             <span class="product_item_price_text">Цена:</span>
             <br>
-            <span class="product_item_price_cost">${formatter(a.price)} <span class="product_item_price_par">₸</span> </span>
-            <a class="product_item_price_btn" data-code="${a.code}">В корзину</a>
+            <span class="product_item_price_cost">${formatter(
+              a.price
+            )} <span class="product_item_price_par">₸</span> </span>
+            <a class="product_item_price_btn" data-code="${
+              a.code
+            }">В корзину</a>
         </div>
         `;
 
-        uniqId.appendChild(productItem);
+    uniqId.appendChild(productItem);
+  });
 
+  document.querySelectorAll(".product_item_price_btn").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const productCode = btn.dataset.code;
+      const product = data.find((item) => item.code === productCode);
+
+      addToCart(product);
     });
-
+  });
 }
+
+// PRODUCTS SORT
+
+const compareCode = (codeA, codeB) => {
+  if (!isNaN(Number(codeA)) && !isNaN(Number(codeB))) {
+    return Number(codeA) - Number(codeB);
+  }
+
+  return codeA.localeCompare(codeB);
+};
+
+const sortingScenarios = {
+  "по возрастанию цены": (a, b) => a.price - b.price,
+  "по убыванию цены": (a, b) => b.price - a.price,
+  "по коду": (a, b) => compareCode(a.code, b.code),
+  "по названию": (a, b) => a.title.localeCompare(b.title),
+};
+
+function sortProducts(products, scenario) {
+  const compare = sortingScenarios[scenario];
+
+  return [...products].sort(compare);
+}
+
+document.querySelectorAll(".sorting_option li").forEach((item) => {
+  item.addEventListener("click", () => {
+    const scenario = item.textContent.trim();
+    const sortedProducts = sortProducts(data, scenario);
+
+    displayList(sortedProducts, computerChairList);
+  });
+});
+
+//  STORE CART PRODUCT
+
+const badge = document.querySelector(".open_cart_number");
+
+function updateBadge() {
+  const cart = localStorage.getItem("cart")
+    ? JSON.parse(localStorage.getItem("cart"))
+    : [];
+  badge.textContent = "";
+  let totalProductsQnty = cart.reduce(
+    (sum, product) => sum + product.quantity,
+    0
+  );
+  badge.textContent = totalProductsQnty;
+}
+
+displayList(data, computerChairList);
+const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addToCart(product) {
+  const existedProduct = cart.find((item) => item.code === product.code);
+
+  if (existedProduct) {
+    existedProduct.quantity += 1;
+  } else {
+    cart.push({ ...product, quantity: 1 });
+  }
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  updateBadge();
+}
+
+updateBadge();
--- js/jquery.cart.js
+++ js/jquery.cart.js
@@ -1,108 +1,158 @@
+const btnLocation = document.getElementById("open_cart_btn");
 
-let btnLocation = document.getElementById('open_cart_btn');
+// utils
+const getCartProducts = () => JSON.parse(localStorage.getItem("cart")) || [];
 
-function formatterCart (priceSum) {
-    let price = priceSum.toString();
-    let formattedPrice = '';
-    for (let i = 0; i < price.length; i++) {
-        if (i > 0 && i % 3 === 0) {
-            formattedPrice = ' ' + formattedPrice;
-        }
-        formattedPrice = price[price.length - 1 - i] + formattedPrice;
-    }
-    return formattedPrice;
+const saveCartProducts = (cartProducts) => {
+  localStorage.setItem("cart", JSON.stringify(cartProducts));
 };
 
-btnLocation.addEventListener('click', function () {
+function formatterCart(priceSum) {
+  let price = priceSum.toString();
+  let formattedPrice = "";
+  for (let i = 0; i < price.length; i++) {
+    if (i > 0 && i % 3 === 0) {
+      formattedPrice = " " + formattedPrice;
+    }
+    formattedPrice = price[price.length - 1 - i] + formattedPrice;
+  }
+  return formattedPrice;
+}
 
-    const divElement = document.createElement('div');
+function calculateTotalPrice(cartProducts) {
+  return cartProducts.reduce(
+    (sum, product) => sum + product.price * product.quantity,
+    0
+  );
+}
 
-    divElement.classList.add('jqcart_layout');
+function renderCartProductsHTML(cartProducts) {
+  const allCartProducts = cartProducts.map((product) => {
+    return `
+      <ul class="jqcart_tbody" data-id="${product.code}">
+        <li class="jqcart_small_td">
+          <img src="${product.img}" alt="${product.title}">
+        </li>
+        <li>
+          <div class="jqcart_nd">
+            <a href="${product.link}">${product.title}</a>
+          </div>
+        </li>
+        <li></li>
+        <li class="jqcart_price">${formatterCart(product.price)} тг</li>
+        <li>
+          <div class="jqcart_pm">
+            <input type="text" class="jqcart_amount" value="${
+              product.quantity
+            }">
+            <span class="jqcart_incr" data-incr="1" data-code=${product.code}>
+              <i class="fa fa-angle-up" aria-hidden="true"></i>
+            </span>
+            <span class="jqcart_incr" data-incr="-1" data-code=${product.code}>
+              <i class="fa fa-angle-down" aria-hidden="true"></i>
+            </span>
+          </div>
+        </li>
+        <li class="jqcart_sum">${formatterCart(
+          product.price * product.quantity
+        )} тг</li>
+      </ul>
+    `;
+  });
 
-    divElement.innerHTML = `
-        <div class="jqcart_content">
-            <div class="jqcart_table_wrapper">
-                <div class="jqcart_manage_order">
-                
-                    <ul class="jqcart_thead">
-                        <li></li>
-                        <li>ТОВАР</li>
-                        <li></li>
-                        <li>ЦЕНА</li>
-                        <li>КОЛИЧЕСТВО </li>
-                        <li>СТОИМОСТЬ</li>
-                    </ul>
-                    
-                    <ul class="jqcart_tbody" data-id="4561">
-                        <li class="jqcart_small_td">
-                            <img src="images/stul_kresla/GloryDC.png" alt="Img">
-                        </li>
-                        <li>
-                            <div class="jqcart_nd">
-                                <a href="#chair.html">Slim DC</a>
-                            </div>
-                        </li>
-                        <li></li>
-                        <li class="jqcart_price">83 000</li>
-                        <li>
-                            <div class="jqcart_pm">
-                                <input type="text" class="jqcart_amount" value="2">
-                                <span class="jqcart_incr" data-incr="1">
-                                    <i class="fa fa-angle-up" aria-hidden="true"></i>
-                                </span>
-                                <span class="jqcart_incr" data-incr="-1">
-                                    <i class="fa fa-angle-down" aria-hidden="true"></i>
-                                </span>
-                            </div>
-                        </li>
-                        <li class="jqcart_sum">166 000 тг</li>
-                    </ul>
-                    
-                    <ul class="jqcart_tbody" data-id="6203">
-                        <li class="jqcart_small_td">
-                            <img src="images/stul_kresla/Hi-tech.png" alt="Img">
-                        </li>
-                        <li>
-                            <div class="jqcart_nd">
-                                <a href="#chair.html">Hi-tech</a>
-                            </div>
-                        </li>
-                        <li></li>
-                        <li class="jqcart_price">95 500</li>
-                        <li>
-                            <div class="jqcart_pm">
-                                <input type="text" class="jqcart_amount" value="1">
-                                <span class="jqcart_incr" data-incr="1">
-                                    <i class="fa fa-angle-up" aria-hidden="true"></i>
-                                </span>
-                                <span class="jqcart_incr" data-incr="-1">
-                                    <i class="fa fa-angle-down" aria-hidden="true"></i>
-                                </span>
-                            </div>
-                        </li>
-                        <li class="jqcart_sum">95 500 тг</li>
-                    </ul>
-                    
-                </div>
-            </div>
-            
-            <div class="jqcart_manage_block">
-                <div class="jqcart_btn">
-                    <button class="jqcart_open_form_btn">Оформить заказ</button>
-                    <form class="jqcart_order_form" style="opacity: 0">
-                        <input class="jqcart_return_btn" type="reset" value="Продолжить покупки">
-                    </form>
-                </div>
-                <div class="jqcart_subtotal">Итого: <strong>261 500</strong> тг</div>
-            </div>
-            
+  return allCartProducts.join("");
+}
+
+function renderCartModal() {
+  const cartProducts = getCartProducts();
+  const totalPrice = calculateTotalPrice(cartProducts);
+
+  const productCartModal = document.createElement("div");
+  productCartModal.classList.add("jqcart_layout");
+  productCartModal.innerHTML = `
+      <div class="jqcart_content">
+        <div class="jqcart_table_wrapper">
+          <div class="jqcart_manage_order">
+            <ul class="jqcart_thead">
+              <li></li>
+              <li>ТОВАР</li>
+              <li></li>
+              <li>ЦЕНА</li>
+              <li>КОЛИЧЕСТВО</li>
+              <li>СТОИМОСТЬ</li>
+            </ul>
+            ${renderCartProductsHTML(cartProducts)}
+          </div>
         </div>
+        <div class="jqcart_manage_block">
+          <div class="jqcart_btn">
+            <button class="jqcart_open_form_btn">Оформить заказ</button>
+            <form class="jqcart_order_form" style="opacity: 0">
+              <input class="jqcart_return_btn" type="reset" value="Продолжить покупки">
+            </form>
+          </div>
+          <div class="jqcart_subtotal">Итого: <strong>${formatterCart(
+            totalPrice
+          )}</strong> тг</div>
+        </div>
+      </div>
     `;
 
-    document.body.appendChild(divElement);
+  productCartModal.addEventListener("click", (e) => {
+    if (e.target === productCartModal) {
+      productCartModal.remove();
+    }
+  });
 
-    document.querySelector('.jqcart_layout').addEventListener('click', function () {
-        document.querySelector('.jqcart_layout').remove();
+  return productCartModal;
+}
+
+function renderModal() {
+  const modal = document.querySelector(".jqcart_layout");
+
+  if (modal) {
+    modal.remove();
+  }
+
+  const cartModal = renderCartModal();
+  document.body.appendChild(cartModal);
+
+  document.querySelectorAll(".jqcart_incr").forEach((button) => {
+    button.addEventListener("click", () => {
+      const increment = parseInt(button.dataset.incr, 10);
+      const productCode = button.dataset.code;
+      console.log(increment, productCode);
+
+      if (increment === 1) {
+        incrementQty(productCode);
+      } else if (increment === -1) {
+        decrementQty(productCode);
+      }
     });
+  });
+}
 
+function incrementQty(productCode) {
+  const cartProducts = getCartProducts();
+  const product = cartProducts.find((p) => p.code === productCode);
+  if (product) {
+    product.quantity += 1;
+    saveCartProducts(cartProducts);
+    renderModal();
+  }
+}
+
+function decrementQty(productCode) {
+  const cartProducts = getCartProducts();
+  const product = cartProducts.find((p) => p.code === productCode);
+  if (product && product.quantity > 1) {
+    product.quantity -= 1;
+    saveCartProducts(cartProducts);
+    renderModal();
+  }
+}
+
+btnLocation.addEventListener("click", () => {
+  console.log("click!!!");
+  renderModal();
 });
